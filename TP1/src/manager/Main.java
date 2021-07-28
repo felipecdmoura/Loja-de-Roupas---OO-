5d9d86ac3cd06d58fbae0f64685d6cac0966c80d
FelpMra <62444107+FelpMra@users.noreply.github.com>
2021-07-28 20:37:48 -0300

    Update 2 - Opções 4 e 5 do menu.

--- TP1/src/manager/Main.java
+++ TP1/src/manager/Main.java
@@ -3,13 +3,18 @@ package manager;
 
 public class Main {
 	private static Client person[] = new Client[10000];
-	private static int pos = 0;
+    private static Product product[] = new Product[10000];
+	private static int posClient = 0;
+    private static int posProd =0;
 	
 	public static void main(String[] args) {
 		int opt;
 		
         for(int count=0;count<10000;count++)
             person[count] = new Client();
+
+        for(int count=0;count<10000;count++)
+            product[count] = new Product();
 
 		do {
 			menu();
@@ -18,20 +23,37 @@ public class Main {
 			switch (opt) {
 			case 1:
 				clear();
-				register();
+				registerClient();
 				break;
             case 2:
                 clear();
                 searchClient();
                 break;
-			case 3: 
+            case 3:
+                clear();
+                registerProd();
+                break;
+            case 4:
+                clear();
+                searchProd();
+                break;
+			case 5: 
 				clear();
-				for(int count=0; count<pos; count++ ) {
+				for(int count=0; count<posClient; count++ ) {
 					System.out.println(person[count].name);
 					System.out.println(person[count].address);
 					System.out.println(person[count].tel);
 				}
 				break;
+            case 6:
+                clear();
+                for(int count=0; count<posProd; count++ ) {
+					System.out.println(product[count].name);
+					System.out.println(product[count].descrip);
+					System.out.println(product[count].price);
+                    System.out.println(product[count].profit);
+                    System.out.println(product[count].stock);
+				}
             case 7:
                 break;
 			default:
@@ -58,13 +80,13 @@ public class Main {
 		}
 	}
 	
-	private static void register() {
+	private static void registerClient() {
 		int qtt;
 		
 		System.out.println("Quantos clientes deseja cadastrar?: ");
 		qtt = Read.getInt();
 
-		for (int count=pos; count<(pos + qtt); count++) {
+		for (int count=posClient; count<(posClient + qtt); count++) {
             clear();
 			System.out.println("Digite o nome do cliente " + (count + 1) + ":");
 			person[count].name = Read.getString();
@@ -75,7 +97,7 @@ public class Main {
 			System.out.println("Digite o telefone do cliente " + (count + 1) + ":");
 			person[count].tel = Read.getString();
 		}
-        pos += qtt;
+        posClient += qtt;
 	}
 
     private static void searchClient() {
@@ -87,7 +109,7 @@ public class Main {
         System.out.println("Insira o nome do cliente a ser pesquisado: ");
         String inputName = Read.getString();
 
-        for(int count=0; count<pos; count++){
+        for(int count=0; count<posClient; count++){
             if(person[count].name.equals(inputName)){
                 verifier = 1;
                 loc = count;
@@ -142,6 +164,114 @@ public class Main {
         }else{
             System.out.println();
             System.out.println("Cliente não encontrado!");;
+        } 
+    }
+
+    private static void registerProd() {
+		int qtt;
+		
+		System.out.println("Quantos produtos deseja cadastrar?: ");
+		qtt = Read.getInt();
+
+		for (int count=posProd; count<(posProd + qtt); count++) {
+            clear();
+			System.out.println("Digite o nome do produto " + (count + 1) + ":");
+			product[count].name = Read.getString();
+			
+			System.out.println("Digitea a descrição do produto " + (count + 1) + ":");
+			product[count].descrip = Read.getString();
+
+            System.out.println("Digite o valor de compra do produto " + (count + 1) + ":");
+			product[count].price = Read.getFloat();
+
+            System.out.println("Digite a porcentagem de lucro do produto " + (count + 1) + ":");
+			product[count].profit = Read.getFloat();
+
+            System.out.println("Digite a quantidade em estoque do produto " + (count + 1) + ":");
+			product[count].stock = Read.getInt();
+		}
+        posProd += qtt;
+	}
+
+    private static void searchProd() {
+        int verifier = 0;
+        int loc = 0;
+        int num = 0;
+        char opt = '0';
+
+        System.out.println("Insira o nome do produto a ser pesquisado: ");
+        String inputName = Read.getString();
+
+        for(int count=0; count<posProd; count++){
+            if(product[count].name.equals(inputName)){
+                verifier = 1;
+                loc = count;
+                break;
+            }else{
+                verifier = 0;
+            }
+        }
+
+        if(verifier == 1){
+            do{
+                clear();
+                System.out.println("Dados do Produto: " );
+                System.out.println("(1)Nome: " + product[loc].name);
+                System.out.println("(2)Descrição: " + product[loc].descrip);
+                System.out.println("(3)Valor de Compra: " + product[loc].price);
+                System.out.println("(4)Porcentagem de Lucro: " + "%" + product[loc].profit);
+                System.out.println("(5)Qtd. em Estoque: " + product[loc].stock);
+                System.out.println();
+                System.out.println("Deseja alterar algum dado cadastrado?(S/N)");
+                opt = Read.getChar();
+                
+            
+                if(opt == 'S' || opt == 's'){
+                    do{
+                        System.out.println("Digite o número correspondente ao dado alterado: ");
+                        num = Read.getInt();
+                        if(num<1 || num>5)
+                            System.out.println("Opção Inválida!");
+                            System.out.println();
+                    }while(num<1 || num>5);
+                        
+                    switch(num){
+                    case 1:
+                        System.out.println("Nova informação: ");
+                        String newName = Read.getString();
+                        product[loc].name = newName;
+                        break;
+                    case 2:
+                        System.out.println("Nova informação: ");
+                        String newDescript = Read.getString();
+                        product[loc].descrip = newDescript;
+                        break;
+                    case 3:
+                        System.out.println("Nova informação: ");
+                        float newPrice = Read.getFloat();
+                        product[loc].price = newPrice;
+                        break;
+                    case 4:
+                        System.out.println("Nova informação: ");
+                        float newProfit = Read.getFloat();
+                        product[loc].profit = newProfit;
+                        break;
+                    case 5:
+                        System.out.println("Nova informação: ");
+                        int newStock = Read.getInt();
+                        product[loc].stock = newStock;
+                        break;
+                    default:
+                        break;
+                    }
+                }else if (opt == 'N' || opt == 'n') {
+                    clear();
+                    break;
+                }
+            }while(opt != 'S' || opt != 's' || opt != 'N' || opt != 'n');
+        }else{
+            System.out.println();
+            System.out.println("Produto não encontrado!");;
         } 
     }
 }
---
+++ TP1/src/manager/Product.java
@@ -0,0 +1,9 @@
+package manager;
+
+public class Product {
+    String name;
+    String descrip;
+    float price;    
+    float profit;
+    int stock;
+}
