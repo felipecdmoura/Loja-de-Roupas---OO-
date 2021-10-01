26d1121529a85c591b9161f3b38637ccc8ca4ac3
Felipe Moura <felipecandidodemoura@gmail.com>
2021-10-01 13:56:21 -0300

    Exclusao da pasta tp1 (para otimizacao do que e necessario); Renomeacao da pasta TP3 para TP4.

--- TP1/src/manager/Client.java
+++
@@ -1,7 +0,0 @@
-package manager;
-
-public class Client { //Classe para agrupar informações dos clientes, onde cada um possui nome, endereço e telefone.
-	String name;
-	String address;
-	String tel;
-}
